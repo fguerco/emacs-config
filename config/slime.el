c5b92ba490533a52fc6649b1fa11aa57c6caedd5
Felipe Guerço Oliveira <felipeguerco@pm.me>
2023-07-19 11:39:47 -0400

    load slime-helper.el only if it exists

--- config/slime.el
+++ config/slime.el
@@ -1,6 +1,9 @@
 ;; -*- lexical-binding: t; -*-
 
-(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+(let ((helper-file (expand-file-name "~/.quicklisp/slime-helper.el")))
+  (when (file-exists-p helper-file)
+    (load helper-file)))
+
 ;; Replace "sbcl" with the path to your implementation
 (setq inferior-lisp-program "sbcl")
 
