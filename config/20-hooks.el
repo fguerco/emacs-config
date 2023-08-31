f0918649202646ac7611e84649c57a19725db698
Felipe Guerço Oliveira <felipeguerco@pm.me>
2023-08-31 15:37:07 -0400

    add prettify-symbols-mode to tall prog-mode

--- config/20-hooks.el
+++ config/20-hooks.el
@@ -2,14 +2,14 @@
 
 (let ((load-lisp
        (lambda ()
-         (prettify-symbols-mode)
          (rainbow-delimiters-mode))))
 
   (add-hook 'prog-mode-hook
             (lambda ()
+              (prettify-symbols-mode)
               (company-mode)
               (display-fill-column-indicator-mode)
               (setq fill-column 80)))
 
-  (dolist (hook '(scheme-mode-hook lisp-mode-hook))
+  (dolist (hook '(scheme-mode-hook lisp-mode-hook emacs-lisp-mode-hook))
     (add-hook hook load-lisp)))
