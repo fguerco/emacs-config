b0caf8ffbac9418fc1357b21c27ce05e43c6de8f
Felipe Guerço Oliveira <felipeguerco@pm.me>
2023-08-31 16:21:16 -0400

    custom commands to create scratch buffers

---
+++ config/commands.el
@@ -0,0 +1,15 @@
+;; -*- lexical-binding: t; -*-
+
+(defmacro define-scratch-command (name major-mode)
+  "Macro to define commands to create scratch buffers with a major mode"
+  (let* ((name (concat (symbol-name name) "-scratch"))
+         (buffer-name (concat "*" name "*")))
+    `(defun ,(intern name) ()
+       (interactive)
+       (switch-to-buffer (get-buffer-create ,buffer-name))
+       (,major-mode))))
+
+
+(define-scratch-command ruby ruby-mode)
+(define-scratch-command python python-mode)
+(define-scratch-command haskell haskell-mode)
